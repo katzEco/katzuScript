bc5baed4ed5f611a017450eed73f15298c2dbde9
dethMastery <me@suphakit.net>
2023-02-24 00:22:49 +0700

    feat: setup paper.jar downloading

--- paper.sh
+++ paper.sh
@@ -4,39 +4,26 @@ ROOT=$HOME/
 FOLDER=server
 SERVERFILE=$ROOT$FOLDER/paper.jar
 
-# if ! [ -x "$(command -v node -v)" ]; then
-#   echo '"Node" is not installed'
-#   echo 'Trying to install "Node"'
-# else
-#   echo '"Node" is already install'
-#   echo ''
-#   echo 'Try checking "hydr"'
-#   echo ''
-
-#   if ! [ -x "$(command -v npx hydr)" ]; then
-#     echo '"hydr" is not installed'
-#     echo ''
-#     echo 'Trying to install "hydr"'
-#     sudo npm i -g hydr
-#   else
-#     echo '"hydr" is already exist!'
-#     echo 'Preparing for download paper.jar..'
-#   fi
-# fi
-
-echo ' '
-
 if [ -f "$FILE" ]; then
   echo '"Server" Folder is already exist.'
+  echo ' '
 else
   mkdir -p -- "$ROOT$FOLDER"
   echo '"Server" Folder is created'
+  echo ' '
 fi
 
 cd $ROOT$FOLDER
 
-# if [[ -f "$SERVERFILE" ]]; then
-#   echo "'Paper.jar is exist!'"
-# else
-  
-# fi
+if [[ -f "$SERVERFILE" ]]; then
+  echo "'Paper.jar is exist!'"
+  echo ' '
+else
+  echo '"paper.jar" is not exist!'
+  echo 'Prepare for loading..'
+  echo ' '
+  curl https://api.papermc.io/v2/projects/paper/versions/1.19.3/builds/418/downloads/paper-1.19.3-418.jar --output paper.jar
+  echo ' '
+  echo '"paper.jar" is finished loading'
+  echo ' '
+fi
