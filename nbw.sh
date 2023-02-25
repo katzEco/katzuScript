2bd64d6130c26c514079f5e365e1ecad8bfee992
dethMastery <me@suphakit.net>
2023-02-26 00:27:40 +0700

    feat: set up workflows script

---
+++ nbw.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+WORKFLOW=".github/workflows"
+
+mkdir -p -- "$WORKFLOW"
+echo 'Created Github Workflows Folder'
+
+cd $WORKFLOW
+curl https://raw.githubusercontent.com/katzEco/katzuScript/main/source/workflowsFile/nodeBuild.yml --output nodeBuild.yml
+echo 'Workflows is set!'
