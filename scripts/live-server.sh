e9624a7550bf270fd35710b15e0560bf7ad41205
Suphakit P <me@suphakit.net>
2023-07-21 02:33:46 +0700

    chore<scripts>: updating script runtime setting up

--- scripts/live-server.sh
+++ scripts/live-server.sh
@@ -4,4 +4,4 @@ echo "setting up `live-server` server config..."
 
 curl https://raw.githubusercontent.com/katzEco/katzuScript/main/source/live-server/server.sh --output live-server
 
-echo 'ko_fi: ' >> .github/FUNDING.yml
+echo "finished.."
