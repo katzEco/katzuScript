cf2adb1ff0bcbf980bec1541d4528189f42d9a40
Suphakit P <me@suphakit.net>
2023-07-21 02:35:07 +0700

    chore<scripts>: fixed typo

--- scripts/live-server.sh
+++ scripts/live-server.sh
@@ -4,4 +4,4 @@ echo "setting up `live-server` server config..."
 
 curl https://raw.githubusercontent.com/katzEco/katzuScript/main/source/live-server/server.sh --output live-server
 
-echo "finished.."
+echo "Finished.."
